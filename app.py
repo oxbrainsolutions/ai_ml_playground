74cc21e84f700719233b9a02cc21699e3980a407
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-03 14:11:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -791,7 +791,7 @@ with col2:
   '''
   st.markdown(header_media_query + header_text, unsafe_allow_html=True)
   information_text1 = '''
-    <p class="information_text" style="margin-top: 2em; margin-bottom: 0em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">The AI & ML playground is an interactive educational tool that provides a hands-on experience with AI technology. A playground is a safe and secure environment where you can experiment and learn without the risk of damaging your production systems. The purpose of this playground is to offer nontechnical individuals the opportunity to engage with AI technology, experiment with different algorithms and gain a deeper understanding of how AI works. To begin, simply select from one of the options below.</span></p>
+    <p class="information_text" style="margin-top: 2em; margin-bottom: 4em; text-align: justify;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1em; ">The AI & ML playground is an interactive educational tool that provides a hands-on experience with AI technology. A playground is a safe and secure environment where you can experiment and learn without the risk of damaging your production systems. The purpose of this playground is to offer nontechnical individuals the opportunity to engage with AI technology, experiment with different algorithms and gain a deeper understanding of how AI works. To begin, simply select from one of the options below.</span></p>
   '''
   subheader_text_field2 = st.empty()
   subheader_text_field2.markdown(information_media_query + information_text1, unsafe_allow_html=True)
