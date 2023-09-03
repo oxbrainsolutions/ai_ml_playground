be58671e235c391b059cb12d9a1c0b7eb3ebaf08
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-03 13:49:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -806,11 +806,7 @@ from st_clickable_images import clickable_images
 
 clicked = clickable_images(
     [
-        "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
-        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
-        "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
-        "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
-        "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
+        "images/facial_detection_transformation.png",
     ],
     titles=[f"Image #{str(i)}" for i in range(5)],
     div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
