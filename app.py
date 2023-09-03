e57d1b30e4f34fb6872dffc0e26355d17495dd18
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-03 12:55:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -772,7 +772,7 @@ spinner = st.empty()
 col1, col2, col3 = st.columns([1, 4, 1])
 with col2:
   header_text = '''
-    <p class="header_text" style="margin-top: 3.6em; margin-bottom: 0em; text-align: center;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1.8em; ">AI & ML Playground</span></p>
+    <p class="header_text" style="margin-top: 4em; margin-bottom: 0em; text-align: center;"><span style="color: #FAFAFA; font-family: sans-serif; font-size: 1.8em; ">AI & ML Playground</span></p>
   '''
 
   header_media_query = '''
